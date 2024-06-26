07748dada28479641be7fe96b5e7a79f0479737b
Akhmad Qasim <akhmadqasim@gmail.com>
2024-06-26 21:59:15 +0800

    initial commit

--- frandi/myWeb/myWeb/urls.py
+++ frandi/myWeb/myWeb/urls.py
@@ -30,3 +30,4 @@ urlpatterns = [
 ###################### ini untuk menampilkan gambar yang sudah di uploud pada folder media #################
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
